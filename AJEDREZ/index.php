4e3465d9ad5dd04cecfc10dd67381c20a0a1217a
Alvaro <alvcomoli@alu.edu.gva.es>
2024-09-23 23:03:04 +0200

    Bona nit1

---
+++ AJEDREZ/index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$filas=8;
+$columnas=8;
+
+for($i=0;$i<=$filas;$i++){
+ print "<tr>";
+    for($j=0;$j<=$columnas;$j++){
+       
+       print "<td></td>";
+    }
+    print "</tr>";
+
+}
+
+
+
+?>
