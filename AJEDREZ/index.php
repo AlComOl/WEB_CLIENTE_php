ae0904aa4af2433edd8941c0b57cb1a90f09272e
Alvaro <alvcomoli@alu.edu.gva.es>
2024-09-24 09:36:22 +0200

    Ajedrez julio

--- AJEDREZ/index.php
+++ AJEDREZ/index.php
@@ -1,19 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Document</title>
+    <script src="index.php"></script>
 
-<?php
+</head>
+<body>
 
-$filas=8;
-$columnas=8;
+    
+    <table>
+        <?php
+        
+        $filas=8;
+        $columnas=8;
+        $color=true;
+        
+        for($i=0;$i<=$filas;$i++){
+         print "<tr>";
+            for($j=0;$j<=$columnas;$j++){
+               if(($i+$j)%2==0){
+                  print '<td class="rojo"></td>';
+               }else{
+                  print '<td class="blanco"></td>';
+               }
+               
+            }
+            print "</tr>";
+        
+        }
+        
+        
+        
+        ?>
+        </table>
 
-for($i=0;$i<=$filas;$i++){
- print "<tr>";
-    for($j=0;$j<=$columnas;$j++){
-       
-       print "<td></td>";
-    }
-    print "</tr>";
 
-}
-
-
-
-?>
+    
+</body>
+</html>
