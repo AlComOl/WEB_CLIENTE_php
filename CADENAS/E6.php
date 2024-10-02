a49fe933771fbbbee791c64ddb06c3c0d3584666
Alvaro <alvcomoli@alu.edu.gva.es>
2024-10-02 22:01:49 +0200

    formulario

---
+++ CADENAS/E6.php
@@ -0,0 +1,13 @@
+<!-- Dado un número cualesquiera, por ejemplo, 1123456789, 
+se trata separar en millares 1.123.456.789, usando funciones de ⦁
+	cadenas. -->
+<php?
+
+$numero=1123456789;
+
+
+
+
+
+
+?>
